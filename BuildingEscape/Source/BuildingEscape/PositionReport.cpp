ca98658a07e12706a605d8e393b468a2cd7a7f02
MTLChrisLEE <sanghyukchrislee@gmail.com>
2018-06-20 23:57:57 -0400

    Created a room
    When the game starts, the door, the table and the chair, each of them
    logs its position and its name.
    The door opens itself by +80.0F degrees (Yaw)
    Added texures(materials) on the walls and the floor

---
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PositionReport.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FString ObjectName = this->GetOwner()->GetName();
+	//FString ObjectPosition = this->GetOwner()->GetActorLocation().ToString();
+	FString ObjectPosition = this->GetOwner()->GetTransform().GetLocation().ToString();
+	
+	UE_LOG(LogTemp, Warning, TEXT("I'm a %s amd I'm at %s"), *ObjectName, *ObjectPosition);
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
